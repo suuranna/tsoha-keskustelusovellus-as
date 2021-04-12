d5fd56d7121edceb76fe19cd785399ce55acd743
Anna Suuronen <annasuuronen@icloud.com>
2021-04-12 20:14:07 +0300

    testi

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///suuranna"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL") #"postgresql:///suuranna"
 db = SQLAlchemy(app)
 app.secret_key = getenv("SECRET_KEY")
 
