7f5b84d4e1cef0b38d3444846a6db6170fa25112
Anna Suuronen <annasuuronen@icloud.com>
2021-05-08 03:46:49 +0300

    sivupyyntöjen ja tietokantakomentojen erottelu

--- functions.py
+++ functions.py
@@ -1,11 +1,65 @@
 from db import db
+from datetime import datetime
 
-#def is_admin(username):
-#    sql = "select admin from users where username=:username"
-#    result = db.session.execute(sql, {"username":username})
+def is_admin(username):
+    sql = "select admin from users where username=:username"
+    result = db.session.execute(sql, {"username":username})
+    admin = result.fetchone()[0]
+    return admin
 
-#def is_their_message(username, message_id):
+def get_password(username):
+    sql = "select password from users where username=:username"
+    result = db.session.execute(sql, {"username":username})
+    password = result.fetchone()
+    return password
 
+def new_user(username, hashed_password):
+    sql = "insert into users (username, password, admin) values (:username, :password, False)"
+    db.session.execute(sql, {"username":username, "password":hashed_password})
+    db.session.commit()
+
+def count_users_with_same_username(username):
+    sql = "select count(*) from users where username=:username"
+    result = db.session.execute(sql, {"username":username})
+    count = result.fetchone()[0]
+    return count
+
+def get_topics():
+    #sql = "select t.id, t.topic, t.public, count(c.topics_id) from topics t, chains c where t.id=c.topics_id group by t.id"
+    sql = "select t.id, t.topic, t.public, count(c.topics_id) from topics t left join chains c on t.id=c.topics_id group by t.id order by t.id desc"
+    result = db.session.execute(sql)
+    topics = result.fetchall()
+    return topics
+
+def create_a_new_chain(topics_id, user_id, title):
+    sql = "insert into chains (topics_id, user_id, title) values (:id, :user_id, :title) returning id"
+    result = db.session.execute(sql, {"id":id, "user_id":user_id, "title":title})
+    chain_id = result.fetchone()[0]
+    db.session.commit()
+    return chain_id
+
+def create_a_new_message(content, user_id, chain_id, begining):
+    sql = "insert into messages (content, user_id, posting_date, begining, chain_id) values (:content, :user_id, datetime.now().strftime(%d/%m/%Y, %H:%M:%S), :begining, :chain_id) returning id"
+    result = db.session.execute(sql, {"content":content, "user_id":user_id, "begining":begining, "chain_id":chain_id})
+    db.session.commit()
+
+def get_the_amount_of_comments_per_topic():
+    sql = "select t.id, count(m.id) from topics t left join chains c on t.id=c.topics_id left join messages m on m.chain_id=c.id and m.begining=False group by t.id"
+    result db.session.execute(sql)
+    comments = result.fetchall()
+    return commentss
+
+#def get_the_amount_of_chains_per_topic():
+#    sql = "select t.id, t.topic, count(c.topics_id) from topics t left join chains c on t.id=c.topics_id group by t.id order by t.id"
+#    result db.session.execute(sql)
+#    chains = result.fetchall()
+#    return chains
+
+def get_the_amount_of_chains_in_one_topic(id):
+    sql = "select count(topics_id) from chains where topics_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    chains = result.fetchall()
+    return chains
 
 def get_user_id(username):
     sql = "select id from users where username=:username"
@@ -13,6 +67,29 @@ def get_user_id(username):
     user_id = result.fetchone()[0]
     return user_id
 
+def get_chains_from_topic(id):
+    #sql = "select c.id, c.title, m.content, m.posting_date from chains c, messages m where m.chain_id=c.id and m.begining=True and c.topics_id=:id"
+    sql = "select c.id, c.title, m.content, m.posting_date, u.username from chains c, messages m, users u where m.chain_id=c.id and m.begining=True and m.user_id=u.id and c.topics_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    chains = result.fetchall()
+    return chains
 
+def get_chain_opening(id):
+    sql = "select c.topics_id, c.id, c.title, m.content, m.posting_date, u.username from chains c, messages m, users u where m.chain_id=c.id and m.begining=True and m.user_id=u.id and c.id=:id"
+    result = db.session.execute(sql, {"id":id})
+    chain_opening = result.fetchone()
+    return chain_opening
+
+def get_comments_of_a_chain(id):
+    sql = "select m.content, u.username, m.posting_date from messages m, users u where u.id=m.user_id and m.begining=False and m.chain_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    comments = result.fetchall()
+    return comments
+
+def get_topic_title(id):
+    sql = "select topic from topics where id=:id"
+    result = db.session.execute(sql, {"id":id})
+    title = result.fetchall()[0]
+    return title
 
 
--- routes.py
+++ routes.py
@@ -6,25 +6,30 @@ import functions
 
 @app.route("/")
 def index():
-    sql = "select id, topic from topics where public=True"
-    result = db.session.execute(sql)
-    topics = result.fetchall()
-    return render_template("index.html",topics=topics)
+    #sql = "select id, topic from topics where public=True"
+    #result = db.session.execute(sql)
+    #topics = result.fetchall() OK OK OK
+    topics = functions.get_topics()
+    comments = functions.get_the_amount_of_comments_per_topic()
+    #chains = functions.get_the_amount_of_chains_per_topic()
+    return render_template("index.html",topics=topics, comments=comments) # chains=chains)
 
 @app.route("/login",methods=["POST"])
 def login():
     username = request.form["username"]
     password = request.form["password"]
-    sql = "select password from users where username=:username"
-    result = db.session.execute(sql, {"username":username})
-    user = result.fetchone()
-    if user == None:
+    #sql = "select password from users where username=:username"
+    #result = db.session.execute(sql, {"username":username})
+    #hashed_password = result.fetchone() OK OK OK
+    hashed_password = functions.get_password(username)
+    if hashed_password == None:
         return render_template("error.html", message="Käyttäjänimi tai salasana on väärä", route="/")
     else:
-        hash_value = user[0]
+        hash_value = hashed_password[0]
         if check_password_hash(hash_value,password):
             session["username"] = username
             session["user_id"] = functions.get_user_id(username)
+            session["admin"] = functions.is_admin(username)
             return redirect("/")
         else:
             return render_template("error.html", message="Käyttäjänimi tai salasana on väärä", route="/")
@@ -37,19 +42,28 @@ def signing_up():
 def sign_up():
     username = request.form["username"]
     password = request.form["password"]
+    password_again = request.form["password_again"]
     if username == "" or password == "":
         return render_template("error.html", message="Syötä käyttäjänimi ja salasana", route="/signing_up")
+    if len(username) > 14 or len(username) < 6:
+       return render_template("error.html", message="Käyttäjänimi ei vastaa pituusvaatimuksia", route="/signing_up")
+    if len(password) > 18 or len(password) < 8:
+       return render_template("error.html", message="Salasana ei vastaa pituusvaatimuksia", route="/signing_up")
+    if password != password_again:
+       return render_template("error.html", message="Salasanat eivät täsmää", route="/signing_up")
     hash_value = generate_password_hash(password)
-    sql = "select count(*) from users where username=:username or password=:password"
-    result = db.session.execute(sql, {"username":username,"password":hash_value})
-    result2 = result.fetchone()[0]
-    if result2 == 0:
-        sql = "insert into users (username, password, admin) values (:username, :password, False)"
-        db.session.execute(sql, {"username":username, "password":hash_value})
-        db.session.commit()
+    #sql = "select count(*) from users where username=:username"
+    #result = db.session.execute(sql, {"username":username})
+    #result2 = result.fetchone()[0] OK OK OK
+    count = functions.count_users_with_same_username(username)
+    if count == 0:
+        functions.new_user(username, hash_value)
+        #sql = "insert into users (username, password, admin) values (:username, :password, False)"
+        #db.session.execute(sql, {"username":username, "password":hash_value})
+        #db.session.commit() OK OK OK
         return redirect("/")
     else:
-        return render_template("error.html", message="Käyttäjätunnus tai salasana on jo käytössä", route="/signing_up")
+        return render_template("error.html", message="Käyttäjätunnus on jo käytössä", route="/signing_up")
 
 @app.route("/logout")
 def logout():
@@ -58,19 +72,22 @@ def logout():
 
 @app.route("/topic/<int:id>")
 def topic(id):
-    sql = "select topic, id from topics where id=:id"
-    result = db.session.execute(sql, {"id":id})
-    topic = result.fetchone()
-    sql = "select id, title from chains where topics_id=:id"
-    result = db.session.execute(sql, {"id":id})
-    chains = result.fetchall()
-    sql = "select content, posting_date, chain_id from messages where begining=True"
-    result = db.session.execute(sql)
-    opening_messages = result.fetchall()
-    sql = "select count(*) from chains where topics_id=:id"
-    result = db.session.execute(sql, {"id":id})
-    amount = result.fetchone()[0]
-    return render_template("topic.html", topic=topic, chains=chains, opening_messages=opening_messages, amount=amount, id=id)
+    #sql = "select topic, id from topics where id=:id"
+    #result = db.session.execute(sql, {"id":id})
+    #topic = result.fetchone()
+    #sql = "select id, title from chains where topics_id=:id"
+    #result = db.session.execute(sql, {"id":id})
+    #chains = result.fetchall() OK OK OK
+    topic = functions.get_topic_title(id)
+    chains = functions.get_chains_from_topic(id)
+    #sql = "select content, posting_date, chain_id from messages where begining=True"
+    #result = db.session.execute(sql)
+    #opening_messages = result.fetchall() OK OK OK
+    #sql = "select count(*) from chains where topics_id=:id"
+    #result = db.session.execute(sql, {"id":id})
+    #amount = result.fetchone()[0] OK OK OK
+    amount = functions.get_the_amount_of_chains_in_one_topic(id)
+    return render_template("topic.html", topic=topic, chains=chains, amount=amount, id=id)
 
 @app.route("/new_comment/<int:id>")
 def new_comment(id):
@@ -82,40 +99,41 @@ def new_chain(id):
 
 @app.route("/create_chain/<int:id>", methods=["POST"])
 def create_chain(id):
-    username = session["username"]
-    sql = "select id from users where username=:username"
-    result = db.session.execute(sql, {"username":username})
-    user_id = result.fetchone()[0]
+    #username = session["username"] Ok
+    #sql = "select id from users where username=:username"
+    #result = db.session.execute(sql, {"username":username})
+    #user_id = result.fetchone()[0] OK OK OK
     content = request.form["content"]
+    title = request.form["title"]
 
     route = "/new_chain/"+str(id)
-    if len(content) > 100:
+    if len(content) > 150:
         return render_template("error.html", message="Aloitusviesti on liian pitkä", route=route)
     if len(content) == 0:
         return render_template("error.html", message="Ketjulta puuttuu aloitusviesti", route=route)
-    title = request.form["title"]
     if len(title) > 50:
         return render_template("error.html", message="Otsikko on liian pitkä", route=route)
     if len(title) == 0:
         return render_template("error.html", message="Ketjulta puuttuu otsikko", route=route)
     if len(title) == 0 and len(content) == 0:
         return render_template("error.html", message="Ei voi julkaista tyhjää ketjua", route=route)
-
-
-    sql = "insert into chains (topics_id, user_id, title) values (:id, :user_id, :title) returning id"
-    result = db.session.execute(sql, {"id":id, "user_id":user_id, "title":title})
-    chain_id = result.fetchone()[0]
-    sql = "insert into messages (content, user_id, posting_date, begining, chain_id) values (:content, :user_id, NOW(), True, :chain_id) returning id"
-    result = db.session.execute(sql, {"content":content, "user_id":user_id, "chain_id":chain_id})
-    db.session.commit()
+    #sql = "insert into chains (topics_id, user_id, title) values (:id, :user_id, :title) returning id"
+    #result = db.session.execute(sql, {"id":id, "user_id":user_id, "title":title})
+    #chain_id = result.fetchone()[0] OK OK OK
+    chain_id = functions.create_a_new_chain(id, session["user_id"], title)
+    functions.create_a_new_message(content, session["user_id"], chain_id, True)
+    #sql = "insert into messages (content, user_id, posting_date, begining, chain_id) values 
+    #       (:content, :user_id, datetime.now().strftime(%d/%m/%Y, %H:%M:%S), True, :chain_id) returning id"
+    #result = db.session.execute(sql, {"content":content, "user_id":user_id, "chain_id":chain_id})
+    #db.session.commit() OK OK OK
     return redirect("/topic/"+str(id))
 
 @app.route("/create_comment/<int:id>", methods=["POST"])
 def create_comment(id):
-    username = session["username"]
-    sql = "select id from users where username=:username"
-    result = db.session.execute(sql, {"username":username})
-    user_id = result.fetchone()[0]
+    #username = session["username"]
+    #sql = "select id from users where username=:username"
+    #result = db.session.execute(sql, {"username":username})
+    #user_id = result.fetchone()[0]
     content = request.form["content"]
 
     route = "/new_comment/"+str(id)
@@ -124,40 +142,43 @@ def create_comment(id):
     if len(content) == 0:
         return render_template("error.html", message="Anna kommentille sisältö", route=route)
 
-    sql = "insert into messages (content, user_id, posting_date, begining, chain_id) values (:content, :user_id, NOW(), False, :id)"
-    result = db.session.execute(sql, {"content":content, "user_id":user_id, "id":id})
-    db.session.commit()
+    #sql = "insert into messages (content, user_id, posting_date, begining, chain_id) values (:content, :user_id, NOW(), False, :id)"
+    #result = db.session.execute(sql, {"content":content, "user_id":user_id, "id":id})
+    #db.session.commit() OK OK OK
+    functions.create_a_new_message(content, session["user_id"], id, False)
     return redirect("/chain/"+str(id))
 
 @app.route("/chain/<int:id>")
 def chain(id):
-    sql = "select user_id from messages where chain_id=:id and begining=True"
-    result = db.session.execute(sql, {"id":id})
-    user_id = result.fetchone()[0]
-    sql = "select username from users where id=:user_id"
-    result = db.session.execute(sql, {"user_id":user_id})
-    username = result.fetchone()[0]
-    sql = "select content, posting_date from messages where chain_id=:id and begining=True"
-    result =db.session.execute(sql, {"id":id})
-    opening_message = result.fetchone()
-    sql = "select title, topics_id from chains where id=:id"
-    result = db.session.execute(sql, {"id":id})
-    title_and_topic_id = result.fetchone()
-    sql = "select content, user_id, posting_date from messages where chain_id=:id and begining=False"
-    result =db.session.execute(sql, {"id":id})
-    messages = result.fetchall()
-    sql = "select id, username from users"
-    result = db.session.execute(sql)
-    usernames = result.fetchall()
-    return render_template("chain.html", id=id, username=username, opening_message=opening_message, title_and_topic_id=title_and_topic_id, messages=messages, usernames=usernames)
+    #sql = "select user_id from messages where chain_id=:id and begining=True"
+    #result = db.session.execute(sql, {"id":id})
+    #user_id = result.fetchone()[0]
+    #sql = "select username from users where id=:user_id"
+    #result = db.session.execute(sql, {"user_id":user_id})
+    #username = result.fetchone()[0]
+    #sql = "select content, posting_date from messages where chain_id=:id and begining=True"
+    #result =db.session.execute(sql, {"id":id})
+    #opening_message = result.fetchone()
+    #sql = "select title, topics_id from chains where id=:id"
+    #result = db.session.execute(sql, {"id":id})
+    #title_and_topic_id = result.fetchone()
+    #sql = "select content, user_id, posting_date from messages where chain_id=:id and begining=False"
+    #result =db.session.execute(sql, {"id":id})
+    #messages = result.fetchall()
+    #sql = "select id, username from users"
+    #result = db.session.execute(sql)
+    #usernames = result.fetchall() OK OK OK
+    chain_opening = functions.get_chain_opening(id)
+    comments = functions.get_comments_of_a_chain(id)
+    return render_template("chain.html", id=id, comments=comments, chain_opening=chain_opening)
 
 @app.route("/result")
 def result():
     query = request.args["query"]
-    sql = "select id, content, user_id, posting_date, chain_id from messages where begining = False and content like :query"
+    sql = "select id, content, user_id, posting_date, chain_id from messages where begining = False and content LIKE :query"
     result = db.session.execute(sql, {"query":"%"+query+"%"})
     messages = result.fetchall()
-    sql = "select id, content, user_id, posting_date, chain_id from messages where begining = True and content like :query"
+    sql = "select id, content, user_id, posting_date, chain_id from messages where begining = True and content LIKE :query"
     result = db.session.execute(sql, {"query":"%"+query+"%"})
     opening_messages = result.fetchall()
     return render_template("result.html", messages=messages, opening_messages=opening_messages)
