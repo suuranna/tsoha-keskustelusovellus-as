823e8e822204b8cf9f47a679f2ae880dfec46772
Anna Suuronen <annasuuronen@icloud.com>
2021-05-09 21:21:46 +0300

    korjauksia

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE chains {
+    id
+}
+
+CREATE TABLE topics {
+    
+}
+
+CREATE TABLE messages {
+    
+}
+
+CREATE TABLE users {
+    
+}
+
+CREATE TABLE private_topics_permissions {
+    
+}
+
